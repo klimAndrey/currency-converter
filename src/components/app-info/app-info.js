1fc7ce7337f42bd9db3239c5d589d59f71f07fd3
klimAndrey <klimenkoandreyska0@gmail.com>
2022-07-13 22:43:04 +0300

    header image 1

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Актуальный курс валют</h1>
+      <h2>Доллар: 1$ - </h2>
+      <h2>Евро: 1€ - </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import AppInfo from "../app-info/app-info";
+import "./app.css";
+
+function App() {
+    return(
+    <div className="app">
+        <AppInfo/>
+    </div>
+    )
+}
+
+export default App;
